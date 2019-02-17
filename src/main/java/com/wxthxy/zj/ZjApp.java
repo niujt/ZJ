a5fbf152234d971261d646d7c621609ae346e4bc
niujt <670482466@qq.com>
2019-02-17 13:37:42 +0800

    firstcommit

---
+++ src/main/java/com/wxthxy/zj/ZjApp.java
@@ -0,0 +1,11 @@
+package com.wxthxy.zj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjApp {
+    public static void main(String[] args){
+        SpringApplication.run(ZjApp.class,args);
+    }
+}
